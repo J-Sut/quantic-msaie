42311369232326563aa8fa1be1f649b86f36b95d
Jeremy Sutton <jeremy.sutton.-nd@disney.com>
2026-08-24 12:04:33 -0700

    f 1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
-alert("uh-oh!")
+alert("uh-oh!");
 console.log("here comes trouble");
 console.log("trivial change 1");
 console.log("trivial change 2");
 console.log("trivial change 3");
+alert("feature 1");
